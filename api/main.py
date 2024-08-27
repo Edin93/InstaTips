a2f5656e73a89aff0c027e9e6b24d4160fb0e1e9
edin93 <il_matador@hotmail.fr>
2024-08-27 18:07:10 +0200

    [init] logging configs for the API

--- api/main.py
+++ api/main.py
@@ -1,13 +1,19 @@
+import logging
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
 from api.database import database
+from api.logging_conf import configure_logging
 from api.routers.post import router as post_router
+
+logger = logging.getLogger(__name__)
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    configure_logging()
+    logger.info("hello world")
     await database.connect()
     yield
     await database.disconnect()
