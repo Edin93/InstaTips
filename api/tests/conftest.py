196b69a56a1c328a4c311c432c723f8ad8910a28
edin93 <il_matador@hotmail.fr>
2024-08-27 06:14:03 +0200

    [add] simple pytests setup, [setup] plain pytest fixtures, [update] the requirements for dev file

---
+++ api/tests/conftest.py
@@ -0,0 +1,31 @@
+from typing import Generator
+
+import pytest
+from fastapi.testclient import TestClient
+from httpx import AsyncClient, AsyncGenerator
+
+from api.main import app
+from api.routers.post import comment_table, post_table
+
+
+@pytest.fixture(scope="session")
+def anyio_backend():
+    return "asyncio"
+
+
+@pytest.fixture()
+def client() -> Generator:
+    yield TestClient(app)
+
+
+@pytest.fixture(autouse=True)
+async def db() -> AsyncGenerator:
+    post_table.clear()
+    comment_table.clear()
+    yield
+
+
+@pytest.fixture()
+async def async_client(client) -> AsyncGenerator:
+    async with AsyncClient(app=app, base_ur=client.base_url) as ac:
+        yield ac
